08ebb236ad0cfaa20625285d33a8196b350efe5f
aqfa11 <80084765+aqfa11@users.noreply.github.com>
2022-04-19 12:42:28 +0700

    ktpaqfacoba

---
+++ src/main/java/projectktpaqfa/projectktpaqfa/coba/DummyJpaController.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projectktpaqfa.projectktpaqfa.coba;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author MSI NOTEBOOK
+ */
+public class DummyJpaController implements Serializable {
+    
+}
